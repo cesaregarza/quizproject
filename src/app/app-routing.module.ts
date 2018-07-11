3506700374606d2f84877e1f6458ce6e7c84192c
Unknown <cesar.garza.826@gmail.com>
2018-07-11 16:11:03 -0500

    Updated Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,33 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { StartpageComponent } from './startpage/startpage.component';
+import { QuestionpageComponent } from './questionpage/questionpage.component';
+import { ResultspageComponent} from './resultspage/resultspage.component';
+
+const routes: Routes = [
+  {
+    path: "",
+    component: StartpageComponent
+  },
+  {
+    path: "questionpage/:id",
+    component: QuestionpageComponent
+  },
+  {
+    path: "resultspage",
+    component: ResultspageComponent
+  },
+  {
+    path: "",
+    redirectTo: "/",
+    pathMatch: "full"
+  }
+]
 
 @NgModule({
   imports: [
-    CommonModule
+    RouterModule.forRoot(routes)
   ],
-  declarations: []
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
