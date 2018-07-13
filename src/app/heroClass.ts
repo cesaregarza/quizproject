928b6fcf459eaad2fa578f4561c1e6fa58e9d34e
Unknown <cesar.garza.826@gmail.com>
2018-07-13 18:34:58 -0500

    Hero List and Class

---
+++ src/app/heroClass.ts
@@ -0,0 +1,4 @@
+export class heroClass {
+    name: string;
+    description: string;
+}
---
+++ src/app/heroList.ts
@@ -0,0 +1,71 @@
+import { heroClass } from "./heroClass";
+
+//THESE ARE ORGANIZED DELIBERATELY, DO NOT MODIFY THE ORDER!!
+//DO NOT ADD OR REMOVE, ONLY MODIFY
+
+export const heroList: heroClass[] = [
+    {
+        name: "Groot",
+        description: ""
+    },
+    {
+        name: "Thor",
+        description: ""
+    },
+    {
+        name: "Vision",
+        description: ""
+    },
+    {
+        name: "Scarlet Witch",
+        description: ""
+    },
+    {
+        name: "Star-Lord",
+        description: ""
+    },
+    {
+        name: "Gamora",
+        description: ""
+    },
+    {
+        name: "Deadpool",
+        description: ""
+    },
+    {
+        name: "Black Panther",
+        description: ""
+    },
+    {
+        name: "Loki",
+        description: ""
+    },
+    {
+        name: "Mantis",
+        description: ""
+    },
+    {
+        name: "Hulk",
+        description: ""
+    },
+    {
+        name: "Dr. Strange",
+        description: ""
+    },
+    {
+        name: "Rocket Raccoon",
+        description: ""
+    },
+    {
+        name: "Thanos",
+        description: ""
+    },
+    {
+        name: "Iron Man",
+        description: ""
+    },
+    {
+        name: "Captain America",
+        description: ""
+    }
+]
