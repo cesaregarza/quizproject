6bdd680648a8d6eeae2ff32789c0098cdb2a06b4
Unknown <cesar.garza.826@gmail.com>
2018-07-16 15:54:25 -0500

    Added Descriptions to Heroes

--- src/app/heroList.ts
+++ src/app/heroList.ts
@@ -6,27 +6,27 @@ import { heroClass } from "./heroClass";
 export const heroList: heroClass[] = [
     {
         name: "Groot",
-        description: ""
+        description: "You are Groot."
     },
     {
         name: "Thor",
-        description: ""
+        description: "You are Thor, the god of thunder! Though you're somewhat of an outsider, you rely on your inner strength rather than tools to help you get ahead. You think rationally under pressure, and are a great leader."
     },
     {
         name: "Vision",
-        description: ""
+        description: "You are Vision! Though your origins gave you a purpose, you're strongly determined to forge your own path independent of that purpose! You're extremely powerful and intelligent, but very humble and friendly."
     },
     {
         name: "Scarlet Witch",
-        description: ""
+        description: "You are the Scarlet Witch! Nobody can explain why you're so powerful, but you are! You've overcome tragedy and misfortune to become a priceless part of whatever team you find yourself on, and are always an asset people can count on."
     },
     {
         name: "Star-Lord",
-        description: ""
+        description: "You are Star-Lord! Though you're a little bit of an outsider, but that fresh perspective makes you think outside of the box: you're not above using the right tools for the job. You always do what's right even if others may not agree with you. Your emotions are very powerful, and drive you to become a great leader!"
     },
     {
         name: "Gamora",
-        description: ""
+        description: "You are Gamora! Though you have a little bit of a tragic past, you're incredibly strong. "
     },
     {
         name: "Deadpool",
